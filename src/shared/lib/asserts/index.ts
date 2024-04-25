1f34f4a2144f1fd9865072cf1cae87a5fc21e01c
tig-bbit <krisato93@hotmail.com>
2024-04-25 18:01:32 +0300

    Fix linting errors

--- src/shared/lib/asserts/index.ts
+++ src/shared/lib/asserts/index.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function invariant(predicate: any, errorMessage: string = 'Assertion Failed'): asserts predicate {
 	if (!predicate) throw new Error(errorMessage);
 }
