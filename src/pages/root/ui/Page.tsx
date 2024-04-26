af836c0bab02fdec674b2c27c686da14e4b9b516
tig-bbit <krisato93@hotmail.com>
2024-04-26 03:30:27 +0200

    update ComingSoonModel visible status using localstorage

--- src/pages/root/ui/Page.tsx
+++ src/pages/root/ui/Page.tsx
@@ -1,21 +1,34 @@
-import { PropsWithChildren} from "react";
+"use client"
+import { PropsWithChildren, useEffect, useState } from "react";
 import { cn } from "~/shared/lib/utils";
 import { ComingSoonModal } from "./ComingSoonModal";
 import { Header } from "./Header";
 
 export function Page({ children }: PropsWithChildren) {
+	const [alertshow, setAlertShow] = useState(false);
+	useEffect(() => {
+		const alertShown = localStorage.getItem('alertShown');
+
+		if (!alertShown) {
+			setAlertShow(true);
+			localStorage.setItem('alertShown', 'true');
+		}
+	}, []);
 	return (
 		<main className={cn(
 			'flex flex-col gap-[1rem] max-w-[90rem] m-auto h-full font-inter',
 			'p-[1.25rem] pt-0 max-md:p-[0.75rem] max-md:pt-0'
 		)}>
 			<Header />
-			
+
 			<div className='grow'>
 				{children}
 			</div>
-
-			<ComingSoonModal />
+			{alertshow ?
+				<ComingSoonModal />
+				:
+				''
+			}
 		</main>
 	);
 }
